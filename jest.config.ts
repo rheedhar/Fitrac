7bd045f437f42ea3d07290fbf67f149206aab400
Fareedat Bello <fareedatbello1@gmail.com>
2024-04-22 23:36:49 -0500

    [Setup] setup jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverage: true,
+    coverageReporters: ['lcov', 'text-summary'],
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/main/main.ts',
+        '!src/main/server.ts',
+    ], // add files that we want to exclude from testing by adding !.
+    coverageThreshold: {
+        global: {
+            statements: 80,
+            branches: 80,
+            functions: 80,
+            lines: 80
+        }
+    }, // coverage settings
+    testPathIgnorePatterns: ['/node_modules/', '/dist/']
+};
+
+export default config;
